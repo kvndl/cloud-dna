e5f56cb8e8a5bcc6d6dd7d004d9ea3d5649802fc
Noah Schepers <admin@noahschepers.com>
2021-08-29 12:14:59 -0400

    Attempt to use SS

--- linode/simple/instance.tf
+++ linode/simple/instance.tf
@@ -1,3 +1,11 @@
+resource "linode_stackscript" "heimdall" {
+  label       = "rocky8-heimdall"
+  description = "Setup Heimdall instance"
+  script      = file("./startup.sh")
+  images      = [var.image]
+  rev_note    = "v1.0"
+}
+
 resource "linode_instance" "heimdall" {
   image           = var.image
   label           = "${var.name}-${var.region}"
@@ -5,6 +13,8 @@ resource "linode_instance" "heimdall" {
   type            = var.type
   authorized_keys = [var.public_ssh_key]
   root_pass       = var.root_pass
+
+  stackscript_id = linode_stackscript.heimdall.id
 
   group = "Terraform"
 }
---
+++ linode/simple/startup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Linode StackScript - Rocky 8
+# Created by: Noah Schepers
+
+# Refs
+OMZ_INSTALL="https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+
+# System - Update
+dnf update -y
+dnf upgrade -y
+
+# System - Install
+dnf install epel-release -y
+dnf install git zsh neofetch -y
+sh -c "$(curl -fsSL "$OMZ_INSTALL")"
+
+# ZSH - Configure
+rm $HOME/.zshrc
+cat << EOF > $HOME/.zshrc
+GPG_TTY=$(tty)
+
+ZSH_THEME="ys"
+
+DISABLE_UPDATE_PROMPT="true"
+
+plugins=(git)
+
+export PATH=$HOME/bin:/usr/local/bin:$PATH:$HOME/.local/bin:$PATH
+
+export ZSH="/$USER/.oh-my-zsh"
+
+export LANG=en_US.UTF-8
+
+export GPG_TTY
+
+source $ZSH/oh-my-zsh.sh
+
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='nano'
+else
+  export EDITOR='subl'
+fi
+
+alias c='clear'
+alias ..='cd ..'
+alias ...='cd ../../'
+EOF
+
+# System - Reboot
+shutdown -r now
